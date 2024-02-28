50c6eb33326f475d12a7625d848bcf3363e0ce16
Mona Rezvani <rezvanii.mona@gmail.com>
2024-02-28 09:52:18 +0100

    refactor

--- src/utilities/useTransformJsonToArray.ts
+++ src/utilities/useTransformJsonToArray.ts
@@ -2,11 +2,11 @@ import { useState } from "react";
 
 export default function useTransformJsonToArray({ response }: any) {
   const [transformedData, setTransformedData] = useState([{}]);
-  let data = response.json();
-  if (!data) return;
-  if (data) {
-    data = JSON.parse(response);
 
+  if (!response) return;
+
+  if (response) {
+    const data = response.json();
     const transformedDataList = [];
     //if we want to have a list of json file we use following
 
